f3cc2c01700804fc48b9adff665e611699d1c393
Dan Beccaria <beccdaniel@gmail.com>
2019-11-06 09:01:56 -0500

    working out opeartions and button actions with JS

---
+++ calc.js
@@ -0,0 +1,22 @@
+let currentTotal = 0;
+let buffer = '0';
+let previousOperator;
+const screen = document.querySelector('.screen');
+
+document.querySelector(".calc-buttons").addEventListener('click', function(event) {
+	
+})
+function getNumber() {
+	button.addEventListener('click', function() {
+		console.log('this was clicked.');
+		console.log(button);
+	});
+}
+
+
+// function getWindow() {
+// 	console.log(this)
+// 	console.log(this.getComputedStyle)
+// }
+// getNumber()
+// getWindow()
