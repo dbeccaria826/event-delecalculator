a7ca5e5b8c5319175c0ebfc6af5f45b0a1f8329c
Dan Beccaria <beccdaniel@gmail.com>
2019-11-06 09:22:58 -0500

    functions can now grab the value of the buttons correctly, was using value instead of inner text

--- calc.js
+++ calc.js
@@ -3,16 +3,36 @@ let buffer = '0';
 let previousOperator;
 const screen = document.querySelector('.screen');
 
-document.querySelector(".calc-buttons").addEventListener('click', function(event) {
-	
-})
-function getNumber() {
-	button.addEventListener('click', function() {
-		console.log('this was clicked.');
-		console.log(button);
-	});
+document.querySelector('.calc-buttons').addEventListener('click', function(event) {
+	console.log(event.target.innerText)
+	buttonClick(event.target.innerText);
+});
+
+function buttonClick(value) {
+	if (isNaN(parseInt(value))) {
+		handleSymbol(value);
+	} else {
+		handleNumber(value);
+	}
+	// rerender();
+}
+function handleNumber(value) {
+	if (buffer === 0) {
+		buffer = value;
+	} else {
+		buffer += value;
+	}
 }
 
+function handleSymbol(value) {
+	if (buffer !== 0) {
+
+	} else {
+
+	}
+}
+//How to check for symbols with built in methods
+console.log(isNaN());
 
 // function getWindow() {
 // 	console.log(this)
